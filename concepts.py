09cf79a453a6ce94c425765d957585b05970e807
pro <pro@prodeMacBook-Pro-2.local>
2023-06-17 16:53:21 +0600

    initialize the system

---
+++ concepts.py
@@ -0,0 +1,20 @@
+'''
+----------------------------
+      High Level Areas
+School system development
+1. Student management system
+2. Teacher management system
+3. Food management
+4. Sports management
+5. Club management
+6. Event Management
+7. Library management
+8. Assessment
+------------------------------
+Core Functionality
+1. There will be a classroom consisting of multiple students
+2. Student can enroll multiple classes
+3. Teachers will take classes, the will take exams, they grade the exams
+4. System will generate the overall grade
+
+'''
